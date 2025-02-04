50964d955b684b1a96270e4f2d77e454c420c73e
elizabethTuzhilina <100961778+elizabethTuzhilina@users.noreply.github.com>
2025-02-04 10:03:57 +0000

    Main Menu and HUD in Game
    Main menu assests turned to sprites and Canvases set up for Main Menu and HUD, SceneLoader script added to call next scene (to start game) and added fonts and converted them to useable font assests in Unity

---
+++ Assets/Nova-Folder/UserInterface/Scripts/SceneManager.cs
@@ -0,0 +1,40 @@
+//Elizabeth Tuzhilina
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+   // loads next scene in the build 
+        public void LoadNextScene()
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                Debug.Log("Error no more scenes" );
+            }
+        }
+
+    // Loads Bedroom
+    public void LoadBedroom()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Loads Rose Garden
+    public void LoadGarden()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Loads Menu  
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
