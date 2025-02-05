8c3af1c588a199b3441c917106a31bbaeeb4922c
AdamKav-student <123458911+AdamKav-student@users.noreply.github.com>
2025-02-05 11:25:37 +0000

    Restoration of files
    Had error with Github now using Gitbash, Recreating old files

---
+++ Assets/Nova-Folder/Sound_Design/Scripts/EnterSound.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterSound : StateMachineBehaviour
+{
+    [SerializeField] private SoundType sound;
+
+    [SerializeField, Range(0, 1)] private float volume = 1;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        AudioManager.PlaySound(sound, null, volume);
+    }
+
+}
---
+++ Assets/Nova-Folder/Sound_Design/Scripts/PlayFootstep.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public void PlaySound()
+{
+    //AudioManager.PlaySound(SoundType.Footsteps);
+    AudioSource audioSource = GetComponent<AudioSource>();
+    if (audioSource != null)
+    {
+        AudioManager.PlaySound(SoundType.Footsteps, audioSource);
+    }
+    else
+    {
+        Debug.LogError("AudioSource component is missing on this GameObject.");
+    }
+}
