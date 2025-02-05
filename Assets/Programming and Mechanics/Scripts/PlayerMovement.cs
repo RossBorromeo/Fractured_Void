b1d29bdc67ce06025b2081663377ec8ca317ca51
RossBorromeo <91060165+RossBorromeo@users.noreply.github.com>
2025-02-05 16:52:27 +0000

    Added Audio Bit for merge

--- Assets/Programming and Mechanics/Scripts/PlayerMovement.cs
+++ Assets/Programming and Mechanics/Scripts/PlayerMovement.cs
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Health Settings")]
     public Health health;
     private bool isDead = false;
+    public AudioSource audioSource; //Adams bit
 
     void Start()
     {
