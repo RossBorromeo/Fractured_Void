ccdc39ac37283d5c5ccf9cd183d2c7338442fe28
elizabethTuzhilina <100961778+elizabethTuzhilina@users.noreply.github.com>
2025-03-28 02:57:50 +0000

    Opening cutscene implemented
     Opening cutscene video is fully integrated into the game and other cut scene and credit scene are created

--- Assets/Nova-Folder/UserInterface/Scripts/SceneLoader.cs
+++ Assets/Nova-Folder/UserInterface/Scripts/SceneLoader.cs
@@ -1,11 +1,34 @@
 //Elizabeth Tuzhilina
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SceneLoader : MonoBehaviour
 {
-   // loads next scene in the build 
-        public void LoadNextScene()
+
+    public VideoPlayer videoPlayer; // VideoPlayer to be assigned in inspector
+
+    void Start()
+    {
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();// trying to find
+        }
+
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoEnd; //  get unity event
+        }
+       
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        LoadNextScene(); // load the next scene when the video ends
+    }
+
+    // loads next scene in the build 
+    public void LoadNextScene()
         {
             int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
             if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
@@ -24,10 +47,16 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    // Loads Bedroom
+    public void LoadCutscen()
+    {
+        SceneManager.LoadScene(2);
+    }
+
     // Loads Rose Garden
     public void LoadGarden()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
 
     // Loads Menu  
